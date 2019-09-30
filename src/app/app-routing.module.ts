cc297079812cfa5bb2c79871adcdbd18091dcafd
Mauricio <mauriciojs.junior@gmail.com>
2019-09-29 23:04:40 -0300

    Cria roteamento para categorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
+import { CategoriesListComponent } from './components/categories/categories-list/categories-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    redirectTo: 'categories',
+    pathMatch: 'full'
+  },
+  {
+    path: 'categories',
+    children: [
+      {
+        path: '',
+        component: CategoriesListComponent
+      }, {
+        path: ':id',
+        component: CategoriesListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
